7c63ce0d9f08793e49a8f848a27aacc3126bc063
Subash Ghimire <62213479+PublisherName@users.noreply.github.com>
2024-08-09 11:32:47 +0545

    Add or update the Azure App Service build and deployment workflow config

--- auths/signals.py
+++ auths/signals.py
@@ -6,7 +6,7 @@ from django_rest_passwordreset.signals import reset_password_token_created
 
 
 @receiver(reset_password_token_created)
-def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+def password_reset_token_created(reset_password_token, *args, **kwargs):
     """
     Handles password reset tokens
     When a token is created, an e-mail needs to be sent to the user
